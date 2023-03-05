4940a4412e053fcbdcf2e268ea9fea0340b94279
deneuvillem <maxime.deneuville@student.uclouvain.be>
2023-03-05 20:18:51 +0000

    handle upstream connection in thread

--- source/extensions/filters/network/echo/echo.cc
+++ source/extensions/filters/network/echo/echo.cc
@@ -19,22 +19,26 @@ namespace Echo {
 
 Network::FilterStatus EchoFilter::onData(Buffer::Instance& data, bool end_stream) {
   ENVOY_CONN_LOG(trace, "echo: got {} bytes", read_callbacks_->connection(), data.length());
-  ENVOY_LOG(debug, "TEST LOG");
-  //std::string str = "Hello world ";
-  //absl::string_view sv = str;
-  //data.prepend(sv);
+  
+  // Print downstream IP + port
+  Network::Connection& connection = read_callbacks_->connection();
+  const auto& stream_info = connection.streamInfo();
+  Network::Address::InstanceConstSharedPtr remote_address = stream_info.downstreamAddressProvider().remoteAddress();
+  std::string source_ip = remote_address->ip()->addressAsString();
+  uint32_t source_port = remote_address->ip()->port();
+  ENVOY_LOG(debug, "Received {} bytes from {}:{}", data.length(), source_ip, source_port);
+
   std::string output = data.toString();
   ENVOY_LOG(debug, output);
-  //read_callbacks_->connection().write(data, end_stream);
   ENVOY_LOG(debug, end_stream);
-  //ASSERT(0 == data.length());
 
+  // Forward requests from downstream to upstream
   int message_length = data.length();
   int bytes_sent = send(sock_, data.toString().c_str(), message_length, 0);
   if (bytes_sent != message_length) {
       ENVOY_LOG(error, "Failed to send message to upstream server");
       data.drain(data.length());
-      return Envoy::Network::FilterStatus::Continue;
+      return Network::FilterStatus::Continue;
   }
 
   data.drain(data.length());
--- source/extensions/filters/network/echo/echo.h
+++ source/extensions/filters/network/echo/echo.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "envoy/network/filter.h"
+#include "envoy/buffer/buffer.h"
+#include "envoy/network/connection.h"
 
 #include "source/common/common/logger.h"
+#include "source/common/buffer/buffer_impl.h"
 
 #include <iostream>
 #include <sys/socket.h>
@@ -10,6 +13,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <cstring>
+#include <thread>
 
 namespace Envoy {
 namespace Extensions {
@@ -58,7 +62,46 @@ public:
     if (connect(sock_, reinterpret_cast<struct sockaddr*>(&upstream_address_), sizeof(upstream_address_)) < 0) {
       throw EnvoyException("Failed to connect to upstream");
     }
+
+  // Display local socket IP + Port
+    struct sockaddr_in source_addr;
+    socklen_t source_len = sizeof(source_addr);
+    getsockname(sock_, reinterpret_cast<struct sockaddr*>(&source_addr), &source_len);
+    ENVOY_LOG(debug, "Socket source IP {}:{}", inet_ntoa(source_addr.sin_addr), std::to_string(ntohs(source_addr.sin_port)));
+
+    // Launch reponses from upstream handler
+    std::thread receive_thread(&EchoFilter::receive_messages, this, sock_);
+    receive_thread.detach();
   }
+
+  // Handle responses received from upstream: send them back to downstream
+  void receive_messages(int socket_fd) {
+    const int BUFFER_SIZE = 1024;
+    char buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    while (true) {
+        bytes_received = recv(socket_fd, buffer, BUFFER_SIZE, 0);
+        if (bytes_received < 0) {
+            ENVOY_LOG(info, "Error receiving message from upstream");
+            break;
+        } else if (bytes_received == 0) {
+            ENVOY_LOG(info, "Upstream closed the connection");
+            break;
+        }
+
+        std::string message(buffer, bytes_received);
+        ENVOY_LOG(info, "Received message from upstream: {}", message);
+
+        Buffer::InstancePtr buffer(new Buffer::OwnedImpl(message));
+        auto& connection = read_callbacks_->connection();
+        if (connection.state() != Network::Connection::State::Closed) {
+          ENVOY_LOG(info, "Sent message to downstream: {}", message);
+          connection.write(*buffer, false);
+        }
+    }
+    close(socket_fd);
+}
 
 private:
   Network::ReadFilterCallbacks* read_callbacks_{};
